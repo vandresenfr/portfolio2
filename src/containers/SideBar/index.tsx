803ef6a56904d014f919e5e0e4263a7ddac8255a
Fernando <fer.vandresen0051@gmail.com>
2024-10-19 19:00:27 -0300

    estilizando a sidebar

--- src/containers/SideBar/index.tsx
+++ src/containers/SideBar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SideBarContainer } from './styles'
 
 const SideBar = () => (
   <aside>
-    <img src="https://github.com/vandresenfr.png" alt="" />
-    <Titulo fontSize={20}>Fernando Lech</Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Fernando Lech</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        vandresenfr
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-End
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
---
+++ src/containers/SideBar/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+import { P } from '../../components/Paragrafo/styles'
+
+export const Descricao = styled(P)`
+  margin-top: 24px;
+  margin-bottom: 40px;
+`
+
+export const BotaoTema = styled.button`
+  font-size: 10px;
+  font-weigth: bold;
+  padding: 8px;
+  color: #eee;
+  border-radius: 12px;
+  background-color: #282a35;
+  cursor: pointer;
+`
+export const SideBarContainer = styled.div`
+  position: sticky;
+  top: 80px;
+  left: 0;
+`
